585f2e2250e749e99af6c1171f91266087a2e638
HAMISH SANSOM <103599187@student.swin.edu.au>
2021-06-29 10:00:13 +1000

    task 1 complete

---
+++ buildquery.sql
@@ -0,0 +1,8 @@
+create database s103599187
+/* Hamish Sansom 103599187
+Subject(SubjCode, Description)
+Teacher(StaffID, Surname, GivenName)
+Student(StudentID, Surname, GivenName, Gender)
+SubjectOffering(SubjCode, StaffID, Year, Semester, Fee)
+Enrolment(SubjCode, StaffID, Year, Semester, StudentID, DateEnrolled, Grade)
+*/
